67c8aa0845eae5faadbc739e7a639cb869ac4eaf
adham cahyo n <79594648+alubeban@users.noreply.github.com>
2021-10-05 17:23:39 +0700

    Add files via upload

---
+++ Contoh FirstConsoleApp_066/Contoh FirstConsoleApp_066/Contoh FirstConsoleApp_066/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Contoh_FirstConsoleApp_066
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
